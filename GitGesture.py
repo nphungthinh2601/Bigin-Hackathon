24f60e084d75fbeb6668db8b50a31ba804df12d9
Toan Ha <toan.ha@bigin.vn>
2023-04-19 22:36:23 +0700

    add some gui

--- GitGesture.py
+++ GitGesture.py
@@ -1,56 +1,81 @@
 import cv2
 import mediapipe as mp
-import subprocess
+import pyautogui
+from tkinter import *
+from PIL import Image, ImageTk
 
+# Set up the pop-up screen
+root = Tk()
+root.title("Hand Gesture Detection")
+root.geometry("1280x720")
+
+# Set up the video window
+label = Label(root)
+label.pack(padx=10, pady=10)
+
+# Initialize the detection module
+mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
-gesture_rec = mp_hands.GestureRecognition('static_gestures', max_num_hands=1)
 
-cap = cv2.VideoCapture(0)
-last_gesture_time = 0
-gesture_timeout = 3
-
-while cap.isOpened():
-    success, image = cap.read()
-
-    if not success:
-        print("Không thể đọc từ camera")
-        break
-
-    with mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.5) as hands:
-        # Chuyển đổi ảnh thành định dạng RGB
+# Define a function to detect the thumb up gesture
+def detect_thumb_up(image):
+    with mp_hands.Hands(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+        
+        # Convert the image to RGB format
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-        # Dò tìm bàn tay trong ảnh
+        
+        # Process the image to detect hands
         results = hands.process(image)
-
+        
+        # Draw hand landmarks on the image
+        annotated_image = image.copy()
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-            # Trực quan hóa các điểm đặc trưng và kết nối tay thành các đường thẳng
-                mp_hands.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-            # Nhận diện cử chỉ
-            gesture_info = gesture_rec.process(image)
+                mp_drawing.draw_landmarks(
+                    annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                
+                # Check if the thumb is up
+                thumb_landmark = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                wrist_landmark = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
+                if (thumb_landmark.y < wrist_landmark.y):
+                    return True
+                
+        # Return False if no thumb up gesture is detected
+        return False
 
-            if gesture_info.gestures:
-                # Lặp qua các cử chỉ được nhận diện
-                for gesture in gesture_info.gestures:
-                # Kiểm tra nếu là cử chỉ thumbs up và hiển thị kết quả
-                    if gesture.name == 'Thumbs Up':
-                        cv2.putText(image, "Thumbs Up", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+# Capture video from the default webcam
+cap = cv2.VideoCapture(0)
 
-                    # Kiểm tra nếu đã quá thời gian timeout giữa các cử chỉ
-                        current_time = cv2.getTickCount()
-                        if current_time >= last_gesture_time + gesture_timeout * cv2.getTickFrequency():
-                            print("Thực hiện hành động Thumbs Up")
-                            subprocess.run(["git", "push"])
-                            last_gesture_time = current_time
-                    else:
-                        cv2.putText(image, "Not Thumbs Up", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+while True:
+    # Read a frame from the video stream
+    ret, frame = cap.read()
+    
+    # Flip the frame horizontally for a mirrored view
+    frame = cv2.flip(frame, 1)
+    
+    # Detect the thumb up gesture
+    if detect_thumb_up(frame):
+        # Perform a left click
+        pyautogui.click()
+        
+        print("Left click performed!")
+    
+    # Convert the image to PIL format and display it in the pop-up screen
+    image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    photo = ImageTk.PhotoImage(image=image)
+    label.config(image=photo)
+    label.image = photo
 
-    cv2.imshow("Thumbs Up Detector", image)
-
-    # Nếu nhấn phím 'q', thoát khỏi vòng lặp
-    if cv2.waitKey(1) == ord("q"):
+    # Update the GUI
+    root.update()
+    
+    # If the user presses "q", break out of the loop and exit
+    if (cv2.waitKey(1) & 0xFF == ord('q')):
         break
 
+# Release the video stream and close all windows
 cap.release()
 cv2.destroyAllWindows()
+root.destroy()
